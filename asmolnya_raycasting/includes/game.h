49e67995d0c04e697c92ac0265ffe66169d7c579
asudie <smolnyakovaae@gmail.com>
2024-12-24 14:51:21 +0100

    started

---
+++ asmolnya_raycasting/includes/game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "../mlx/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#endif
---
+++ asmolnya_raycasting/src/main.c
@@ -0,0 +1,6 @@
+#include "../includes/game.h"
+
+int main()
+{
+	return 0;
+}
