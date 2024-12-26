2c2109622f8a6f9dea513c8a16426cdf75acc97b
asudie <smolnyakovaae@gmail.com>
2024-12-26 20:16:32 +0100

    no segfault but doe not draw shit

--- asmolnya_raycasting/src/main.c
+++ asmolnya_raycasting/src/main.c
@@ -1,6 +1,44 @@
 #include "../includes/game.h"
 
+void put_pixel(int x, int y, int color, t_game *game)
+{
+	if(x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+		return;
+	int index = y * game->size_line + x * game->bpp / 8; // WAT IS DIS???
+	printf("HERE\n");
+	game->data = malloc(sizeof(char) * index);
+	game->data[index] = color & 0xFF;
+	game->data[index + 1] = (color >> 8) & 0xFF; // WAT IS DIS???
+	game->data[index + 2] = (color >> 16) & 0xFF; // WAT IS DIS???
+}
+
+void draw_square(int x, int y, int size, int color, t_game *game)
+{
+	for(int i = 0; i < size; i++)
+		put_pixel(x + i, y, color, game);
+	for(int i = 0; i < size; i++)
+		put_pixel(x, y + i, color, game);
+	for(int i = 0; i < size; i++)
+		put_pixel(x + size, y, color, game);
+	for(int i = 0; i < size; i++)
+		put_pixel(x, y + size, color, game);
+}
+
+void init_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Game");
+	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	// game->data = malloc(sizeof(char *))
+	
+}
+
 int main()
 {
+	t_game *game = malloc(sizeof(t_game));
+	init_game(game);
+	
+	draw_square(WIDTH / 2, HEIGHT / 2, 10, 0x00FF00, game);
+	mlx_loop(game->mlx);
 	return 0;
 }
