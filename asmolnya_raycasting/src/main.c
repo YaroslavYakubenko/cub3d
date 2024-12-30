d32f4336e3b9d508d98bec7525c8fc9bfe3004dc
asudie <smolnyakovaae@gmail.com>
2024-12-30 09:42:08 +0100

    still does not de´raw anything

--- asmolnya_raycasting/src/main.c
+++ asmolnya_raycasting/src/main.c
@@ -29,6 +29,8 @@ void init_game(t_game *game)
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Game");
 	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	game->data = mlx_get_data_addr(game->img, &game->bpp, &game->size_line, &game->endian);
+	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
 	// game->data = malloc(sizeof(char *))
 	
 }
