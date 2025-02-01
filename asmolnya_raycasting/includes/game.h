60e828cf7903be3d5b07ade8935b1a89e59ceeb2
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2025-02-01 15:37:53 +0100

    no fish eye

--- asmolnya_raycasting/includes/game.h
+++ asmolnya_raycasting/includes/game.h
@@ -1,7 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "../mlx/mlx.h"
+#include "../../mlx/mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
--- asmolnya_raycasting/src/main.c
+++ asmolnya_raycasting/src/main.c
@@ -83,6 +83,16 @@ int distance(float x, float y)
 	return sqrt(x * x + y * y);
 }
 
+float fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
+{
+	float delta_x = x2 - x1;
+	float delta_y = y2 - y1;
+	float angle = atan2(delta_y, delta_x) - game->player.angle;
+	float fix_dist = distance(delta_x, delta_y) * cos(angle);
+	return fix_dist;
+
+}
+
 void draw_line(t_player *player, t_game *game, float start_x, int i)
 {
 	float cos_angle = cos(start_x);
@@ -93,11 +103,11 @@ void draw_line(t_player *player, t_game *game, float start_x, int i)
 
 	while(!touch(ray_x, ray_y, game))
 	{
-		put_pixel(ray_x, ray_y, 0xFF0000, game);
+		// put_pixel(ray_x, ray_y, 0xFF0000, game);
 		ray_x += cos_angle;
 		ray_y += sin_angle;
 	}
-	float dist = distance(ray_x - player->x, ray_y - player->y);
+	float dist = fixed_dist(player->x, player->y, ray_x, ray_y, game);
 	float height = (BLOCK / dist) * (WIDTH / 2);
 	int start_y = (HEIGHT - height) / 2;
 	int end = start_y + height;
@@ -114,8 +124,8 @@ int draw_loop(t_game *game)
 	t_player *player = &game->player;
 	move_player(player);
 	clear_image(game);
-	draw_square(player->x, player->y, 10, 0x00FF00, game);
-	draw_map(game);
+	// draw_square(player->x, player->y, 10, 0x00FF00, game);
+	// draw_map(game);
 	// float ray_x = player->x;
 	// float ray_y = player->y;
 	// float cos_angle = cos(player->angle);
